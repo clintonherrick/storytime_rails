1609a25f552cc8eb16b732bbe454de8ac9b8eeaa
brigham thompson & tony navarro <student@epicodus.com>
2015-10-14 11:24:07 -0700

    Add edit functionality to stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -16,13 +16,18 @@ class StoriesController < ApplicationController
     end
   end
 
-  # def edit
-  #
-  # end
-  #
-  # def update
-  #
-  # end
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  def update
+    @story = Story.find(params[:id])
+    if @story.update(story_params)
+      redirect_to stories_path
+    else
+      render :edit
+    end
+  end
 
   private
 
