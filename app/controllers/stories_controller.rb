e3885a9ce928f284ea4cac49153c968b7b47abc5
brigham thompson & tony navarro <student@epicodus.com>
2015-10-14 11:34:16 -0700

    add show path for individual stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,6 +3,10 @@ class StoriesController < ApplicationController
     @stories = Story.all
   end
 
+  def show
+    @story = Story.find(params[:id])
+  end
+
   def new
     @story = Story.new
   end
